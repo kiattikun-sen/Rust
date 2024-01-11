448f7aa6d1a84e43b6a297ef73064b21efce7fe1
kiattikun-sen <win.wiigan@gmail.com>
2024-01-11 08:46:05 +0700

    main.rs
    Edit Source Code

---
+++ variable/target/debug/deps/variable.d
@@ -0,0 +1,5 @@
+C:\Users\kiatt\OneDrive\Documents\GitHub\Rust\variable\target\debug\deps\variable.exe: src\main.rs
+
+C:\Users\kiatt\OneDrive\Documents\GitHub\Rust\variable\target\debug\deps\variable.d: src\main.rs
+
+src\main.rs:
---
+++ variable/target/debug/variable.d
@@ -0,0 +1,1 @@
+C:\Users\kiatt\OneDrive\Documents\GitHub\Rust\variable\target\debug\variable.exe: C:\Users\kiatt\OneDrive\Documents\GitHub\Rust\variable\src\main.rs
